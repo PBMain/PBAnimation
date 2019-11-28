7b45c182a5cd06a9285ddff598b99602c17fb79e
Daniel Spencer <danspencer@photobutler.com>
2019-11-27 18:56:02 -0800

    Initial Commit

---
+++ PBFrameworkAnimation.framework/Headers/PBFrameworkAnimation.h
@@ -0,0 +1,19 @@
+//
+//  PBFrameworkAnimation.h
+//  PBFrameworkAnimation
+//
+//  Created by Daniel Spencer on 11/27/19.
+//  Copyright © 2019 Daniel Spencer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PBFrameworkAnimation.
+FOUNDATION_EXPORT double PBFrameworkAnimationVersionNumber;
+
+//! Project version string for PBFrameworkAnimation.
+FOUNDATION_EXPORT const unsigned char PBFrameworkAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBFrameworkAnimation/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# PBAnimation
+
+## Requirements
+
+## Installation
+
+PBAnimation is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'PBAnimation'
+```
+
+## Author
+
+drewspencer@yourvideokeepsakes.com, danspencer@photobutler.com
+
+## License
+
+PBAnimation is available under the MIT license. See the LICENSE file for more info.
